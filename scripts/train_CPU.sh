77573b4a1df43d2a4ac8b417f4e88fe0eec7206f
josephvalencia <jdv@172.16.47.71>
2020-02-20 17:10:41 -0800

    CPU train script changed paths

--- scripts/train_CPU.sh
+++ scripts/train_CPU.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-source venv/bin/activate
+source ../venv/bin/activate
 export PYTHONPATH=/Users/jdv/Research/bioseq2seq
-python bioseq2seq/bin/train.py --input translations.map --accum_steps 4 --save-directory ./checkpoints4/ --num_gpus 0
+python ../bioseq2seq/bin/train.py --input ../../translations.map --accum_steps 4 --save-directory ./checkpoints4/ --num_gpus 0
